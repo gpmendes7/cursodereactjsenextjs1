a559028e96b1f53af9c5ef3d307b10d2f4690c53
gabriel7mendes <andrewsseven@gmail.com>
2021-06-24 10:24:11 -0300

    hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,42 +1,87 @@
-import { useState } from "react";
-import { useFetch } from "./useFetch";
+import { useCallback, useEffect, useState } from "react";
+
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: "idle",
+  });
+
+  const run = useCallback(async () => {
+    setState({
+      result: null,
+      error: null,
+      status: "pending",
+    });
+
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: "settled",
+        });
+      })
+      .catch((err) => {
+        setState({
+          result: null,
+          error: err,
+          status: "error",
+        });
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
+
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  // throw new Error("Que chato");
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const json = await data.json();
+  return json;
+};
 
 export const Home = () => {
-  const [postId, setPostId] = useState("");
-  const [result, loading] = useFetch(
-    "https://jsonplaceholder.typicode.com/posts/" + postId,
-    {
-      headers: {
-        abc: "1",
-      },
-    }
-  );
+  // eslint-disable-next-line no-unused-vars
+  const [posts, setPosts] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
 
-  if (loading) {
-    return <p>Loading...</p>;
+  useEffect(() => {
+    setTimeout(() => {
+      reFetchData();
+    }, 6000);
+  }, [reFetchData]);
+
+  function handleClick() {
+    reFetchData();
   }
 
-  const handleClick = (id) => {
-    setPostId(id);
-  };
+  if (status === "idle") {
+    return <pre>idle: Nada executando</pre>;
+  }
 
-  if (!loading && result) {
+  if (status === "pending") {
+    return <pre>pending: Loading</pre>;
+  }
+
+  if (status === "error") {
+    return <pre>error: {error.message}</pre>;
+  }
+
+  if (status === "settled") {
     return (
-      <div>
-        {result?.length > 0 ? (
-          result.map((p) => (
-            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
-              <p>{p.title}</p>
-            </div>
-          ))
-        ) : (
-          <div onClick={() => handleClick("")}>
-            <p>{result.title}</p>
-          </div>
-        )}
-      </div>
+      <pre onClick={handleClick}>
+        settled: {JSON.stringify(result, null, 2)}
+      </pre>
     );
   }
 
-  return <h1>Oi</h1>;
+  return "";
 };
